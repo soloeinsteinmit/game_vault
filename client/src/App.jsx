3d62f72b66308563d865524af6013efd70ea8b91
soloeinsteinmit <solomoneinsteinmit300@gmail.com>
2024-06-29 10:30:32 +0000

    Created .env and add my RAWG API key for security

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,13 +7,18 @@ import { Button } from "@nextui-org/button";
 function App() {
   const [count, setCount] = useState(0);
 
+  const rawgApiKey = import.meta.env.VITE_RAWG_API_KEY;
+  console.log(rawgApiKey);
+
   return (
     <NextUIProvider>
-      <NextThemesProvider attribute="class" defaultTheme="light">
-        <div className="flex flex-col justify-center items-center">
-          <h1>GameVault</h1>
-          <Button>Get started with GameVault</Button>
-          <p>Made by Solomon Eshun</p>
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+        <div className="flex flex-col gap-5 justify-center items-center">
+          <h1 className="text-3xl font-bold ">GameVault v1.0</h1>
+          <p>A clone of the website based on the RAWG API</p>
+          <p>My API Key rawgApiKey here</p>
+          <Button color="primary">Get started with GameVault</Button>
+          <span className="text-xs">Made by Solomon Eshun</span>
         </div>
       </NextThemesProvider>
     </NextUIProvider>
